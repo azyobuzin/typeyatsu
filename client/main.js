e20a8215c348934179797fd838c0083f22318282
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-06-08 15:13:52 +0900

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,30 @@
+/// <reference path="../typings/node/node.d.ts"/>
+
+var app = require("app");
+var BrowserWindow = require("browser-window");
+
+var typeRemote = process.env["TYPE_REMOTE"];
+if (!typeRemote) typeRemote = "typesuruyatsu.azyobuzi.net";
+global.remoteServer = typeRemote;
+
+var mainWindow = null;
+
+app.on("ready", function () {
+    mainWindow = new BrowserWindow({ "web-preferences": { "web-security": false } });
+    mainWindow.loadUrl("file://" + __dirname + "/content/index.html");
+    //mainWindow.openDevTools();
+    mainWindow.on("closed", function () { app.quit(); });
+});
+
+var ipc = require("ipc");
+var leafletWindow = null;
+
+ipc.on("openLeaflet", function () {
+    if (leafletWindow) {
+        leafletWindow.focus();
+    } else {
+        leafletWindow = new BrowserWindow({ width: 400, height: 300 });
+        leafletWindow.loadUrl("file://" + __dirname + "/content/leaflet.html");
+        leafletWindow.on("closed", function () { leafletWindow = null; })
+    }
+});
